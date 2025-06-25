552ca4dfccdaec42543efcdb7a92884376cc4358
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-25 16:33:26 -0300

    fix:  Insere imagens novas em Frigorifico e Convencimento

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -7,7 +7,7 @@ import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.jpg";
 
 import imgFeatures from "../assets/imgs/features/imgServices.webp";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
-import frigorifico from "../assets/imgs/features/frigorifico.webp";
+import frigorifico from "../assets/imgs/acidents/frigorifico.webp";
 
 import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import imgAbout1 from "../assets/imgs/about/aboutImg1.jpg";
