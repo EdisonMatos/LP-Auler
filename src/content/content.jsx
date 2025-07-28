fd2155dc0e59fced11eef98206b17608969bd062
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-28 10:36:26 -0300

    fix: Seção adicionada

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -832,7 +832,7 @@ const content = {
       subtitle:
         "Visite nosso endereço e confira no mapa abaixo como chegar facilmente.",
       embedsrc:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8861.372397471707!2d-52.61422465368103!3d-27.09490860615618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e4b69bfc3650df%3A0x55464059f41656aa!2sEscrit%C3%B3rio%20de%20Advocacia!5e1!3m2!1spt-BR!2sbr!4v1750709753100!5m2!1spt-BR!2sbr",
+        "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d899.5383805748156!2d-52.6118644!3d-27.0962987!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e4b69bf94b0027%3A0x14a9710999ab8f79!2sR.%20Minas%20Gerais%2C%20533%20-%20e%20-%20Presidente%20M%C3%A9dici%2C%20Chapec%C3%B3%20-%20SC%2C%2089801-200!5e1!3m2!1spt-BR!2sbr!4v1753708031595!5m2!1spt-BR!2sbr",
     },
     about: {
       imagem: {
@@ -1079,6 +1079,12 @@ const content = {
       },
       paragraph:
         "Se você se identificou com qualquer uma dessas situações, não fique calado e descubra como garantir seus direitos.",
+    },
+    news: {
+      miniTag: "NOTÍCIAS",
+      title: "Últimas notícias sobre Direitos Trabalhistas",
+      subtitle:
+        "Fique por dentro dos principais acontecimentos e denúncias recentes.",
     },
     footer: {
       copyrightLine: ` ${currentYear} - ${infos.name} - Todos os direitos reservados`,
